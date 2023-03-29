a9457abbf2d449c3d6f63984e6625f09f480165d
chandni <chandni.goyal@knimbus.com>
2023-03-29 17:24:34 +0530

    firebase initialise fix

--- helpers/db.py
+++ helpers/db.py
@@ -1,5 +1,6 @@
 import json
 import firebase_admin
+from firebase_admin import credentials
 from google.oauth2 import service_account
 from google.cloud import firestore
 import os
@@ -11,7 +12,7 @@ creds_dict = json.loads(firestore_credentials)
 
 # Create a credentials object from the dictionary
 creds = service_account.Credentials.from_service_account_info(creds_dict)
-app = firebase_admin.initialize_app(credential=creds)
+app = firebase_admin.initialize_app(credential=credentials.Certificate(creds_dict))
 
 # Initialize the Firestore client with the credentials
 db = firestore.Client(credentials=creds)
