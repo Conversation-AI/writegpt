58137a3b1bb506dce69ae8b4a37b5c58b1f9f158
chandni <chandni.goyal@knimbus.com>
2023-05-12 23:00:00 +0530

    stripe usage record modified

--- helpers/record_usage.py
+++ helpers/record_usage.py
@@ -1,4 +1,6 @@
-import stripe, os, time
+import os
+import stripe
+import time
 
 stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
 
@@ -7,10 +9,10 @@ stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
 def update_usage_record_by_user(user):
     subscription_item_id = user.subscription_item_id
     # increment usage by 1
-    stripe.SubscriptionItem.create_usage_record(
+    usage_record = stripe.SubscriptionItem.create_usage_record(
         subscription_item_id,
         quantity=1,
         timestamp=int(time.time()),
     )
 
-    print("usage record added/updated")
+    print("usage record added/updated : ", usage_record)
